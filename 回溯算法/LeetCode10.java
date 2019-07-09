fb3a4bcdbf008fb228acc801910fb307322b3360
Li.Shangzhi <13680348517@163.com>
2019-07-09 14:12:29 +0800

    leetcode 解题

---
+++ 回溯算法/LeetCode10.java
@@ -0,0 +1,26 @@
+/**
+ * @Author: lishangzhi
+ * @Date: 2019/7/9
+ * @Description: <正则表达式匹配>
+ * @link https://leetcode-cn.com/problems/regular-expression-matching/
+ */
+public class LeetCode10 {
+
+    /**
+     * '.' 匹配任意单个字符
+     * '*' 匹配零个或多个前面的那一个元素
+     *
+     * @param s  字符串
+     * @param p  字符规则
+     * @return
+     */
+    public boolean isMatch(String s, String p) {
+
+        return false;
+    }
+
+
+    private void  backtrack(){
+
+    }
+}
--- 回溯算法/LeetCode40.java
+++ 回溯算法/LeetCode40.java
@@ -1,18 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author: lishangzhi
  * @Date: 2019/7/9
- * @Description: <>
- * @Version: v2.0.0
- * @Copy: 深圳市蓝凌软件股份有限公司
+ * @Description: <组合总和 II>
+ * @link https://leetcode-cn.com/problems/combination-sum-ii/
  */
 public class LeetCode40 {
 
-    /**
-     * @link https://leetcode-cn.com/problems/combination-sum-ii/
-     */
+    //数据集合
+    private List<List<Integer>> ans = new ArrayList<List<Integer>>();
 
     /**
      * @param candidates 指定数组
@@ -21,13 +18,17 @@ public class LeetCode40 {
      */
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
 
-        //数据集合
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-
         //组合过程集合
         List<Integer> cans = new ArrayList<Integer>();
+        Arrays.sort(candidates);
+        if (target < candidates[0])
+            return ans;
 
-        backtrack(target, 0, 0, cans, ans);
+        backtrack(candidates, target, 0, 0, cans);
+
+        //由于candidates存在重复数字，ans一定会存在重复集合，利用set集合特性去重
+        Set<List<Integer>> set = new HashSet<>(ans);
+        ans = new ArrayList<>(set);
         return ans;
     }
 
@@ -39,18 +40,31 @@ public class LeetCode40 {
      * @param cans   组合过程集合
      * @param index  下标
      */
-    private void backtrack(int tatget, int sum, int index, List<Integer> cans, List<List<Integer>> ans) {
+    private void backtrack(int[] candidates, int tatget, int sum, int index, List<Integer> cans) {
 
-        //确定回溯点
-        if (sum == tatget) {
-            ans.add(cans);
-            //未移除已经计算的数值
+        //确定结束点
+        if (tatget == 0) {
+            ans.add(new ArrayList<Integer>(cans));
             return;
         }
 
-        if (sum < tatget){
+        for (int i = index; i < candidates.length; i++) {
+            if (candidates[i] <= tatget) {
+                //加上数值
+                cans.add(candidates[i]);
+                backtrack(candidates, tatget - candidates[i], sum, i + 1, cans);
+                //减去数值
+                cans.remove(cans.size() - 1);
+            } else {
+                return;
+            }
 
         }
 
+    }
+
+    public static void main(String[] args) {
+        LeetCode40 leetCode = new LeetCode40();
+        leetCode.combinationSum2(new int[]{2,5,2,1,2}, 5);
     }
 }
