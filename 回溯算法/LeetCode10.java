1c774bc970839f20cee1858b2fc3848e6e2a033c
Li.Shangzhi <13680348517@163.com>
2019-07-10 17:54:54 +0800

    leetcode 解题

--- 回溯算法/LeetCode10.java
+++ 回溯算法/LeetCode10.java
@@ -10,20 +10,25 @@ public class LeetCode10 {
      * '.' 匹配任意单个字符
      * '*' 匹配零个或多个前面的那一个元素
      *
-     * @param s  字符串
-     * @param p  字符规则
+     * @param text    字符串
+     * @param pattern 字符规则
      * @return
      */
-    public boolean isMatch(String s, String p) {
+    public boolean isMatch(String text, String pattern) {
+        //如果字符规则为空,则不存在匹配
+        if (pattern.isEmpty()) return text.isEmpty();
 
-        backtrack(s.length(),p.length(),s,p);
+        boolean first_match = (!text.isEmpty() && (text.charAt(0) == pattern.charAt(0) || pattern.charAt(0) == '.'));
 
-        // 字符长度
-        return false;
+        if (text.length() >= 2 && pattern.charAt(1) == '*') {
+            return (isMatch(text, pattern.substring(2)) || (first_match && isMatch(text.substring(1), pattern)));
+        } else {
+            return first_match && isMatch(text.substring(1), pattern.substring(1));
+        }
     }
 
-
-    private void  backtrack(int slen,int plen,String s,String p){
-
+    public static void main(String[] args) {
+        LeetCode10 leetCode = new LeetCode10();
+        leetCode.isMatch("", "");
     }
 }
---
+++ 深度优先搜索/LeetCode.java
@@ -0,0 +1,9 @@
+/**
+ * @Author: lishangzhi
+ * @Date: 2019/7/9
+ * @Description: <>
+ * @Version: v2.0.0
+ * @Copy: 深圳市蓝凌软件股份有限公司
+ */
+public class LeetCode {
+}
