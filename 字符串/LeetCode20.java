ee3e015df12542a3b699ab583d073a2d24af774a
Li.Shangzhi <13680348517@163.com>
2019-08-05 10:29:12 +0800

    四元组

--- 字符串/LeetCode20.java
+++ 字符串/LeetCode20.java
@@ -1,51 +1,52 @@
 import com.sun.xml.internal.ws.util.StringUtils;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 /**
  * @Description: <>
  * @Author: lishangzhi
  */
 public class LeetCode20 {
 
-    /**
-     * 判断有效字符串
-     *
-     * @param s
-     * @return
-     */
-    public boolean isValid(String s) {
-        //连续闭合纠正
-        char[] ans = s.toCharArray();
-        if (ans == null) return false;
-        return recursion(ans,0);
+    //Hash存储配对
+    private HashMap<Character, Character> mappings;
+
+
+    public LeetCode20() {
+        this.mappings = new HashMap<Character, Character>();
+        this.mappings.put(')', '(');
+        this.mappings.put('}', '{');
+        this.mappings.put(']', '[');
     }
 
-    public boolean recursion(char[] ans, int index) {
-        if (index == ans.length) return true;
-        if (ans[index] == '(' && ans[index + 1] == ')') {
-            index = index + 2;
-            recursion(ans, index);
-        }else {
-            return false;
+    public boolean isValid(String s) {
+
+        //初始化stack
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            //判断是否匹配,不存在则push进入到栈中，否则pop取出栈
+            if (this.mappings.containsKey(c)) {
+
+                //对称机制取出最顶上元素
+                char topElement = stack.empty() ? '#' : stack.pop();
+
+                if (topElement != this.mappings.get(c)) {
+                    return false;
+                }
+            } else {
+                //push 到栈中
+                stack.push(c);
+            }
         }
-        if (index == ans.length) return true;
-        if (ans[index] == '{' && ans[index + 1] == '}') {
-            index = index + 2;
-            recursion(ans, index);
-        }else {
-            return false;
-        }
-        if (index == ans.length) return true;
-        if (ans[index] == '[' && ans[index + 1] == ']') {
-            index = index + 2;
-            recursion(ans, index);
-        }else {
-            return false;
-        }
-        return false;
+        return stack.isEmpty();
     }
 
 
     public static void main(String[] args) {
-        System.out.println(new LeetCode20().isValid("()[]{}"));
+        System.out.println(new LeetCode20().isValid("{{}}[({})]"));
     }
 }
